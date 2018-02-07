f63a23eb6839d77c4bc49d12e34959dd39ca7de9
Ryan Shaw <ryan@rshaw.me>
2018-02-07 08:10:03 +0000

    Update signing and dist

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -16,23 +16,15 @@ angular.module('copayApp.services').factory('configService', function(storageSer
     },
 
     download: {
-      bitpay: {
-        url: 'https://bitpay.com/wallet'
-      },
       copay: {
-        url: 'https://copay.io/#download'
+        url: 'https://garlicoin.io'
       }
     },
 
     rateApp: {
-      bitpay: {
-        ios: 'http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=1149581638&pageNumber=0&sortOrdering=2&type=Purple+Software&mt=8',
-        android: 'https://play.google.com/store/apps/details?id=com.bitpay.wallet',
-        wp: ''
-      },
       copay: {
-        ios: 'http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=951330296&pageNumber=0&sortOrdering=2&type=Purple+Software&mt=8',
-        android: 'https://play.google.com/store/apps/details?id=com.bitpay.copay',
+        ios: 'https://garlicoin.io',
+        android: 'https://garlicoin.io',
         wp: ''
       }
     },
@@ -73,7 +65,7 @@ angular.module('copayApp.services').factory('configService', function(storageSer
     },
 
     release: {
-      url: 'https://api.github.com/repos/bitpay/copay/releases/latest'
+      url: 'https://api.github.com/repos/garlicoin-project/copay-grlc/releases/latest'
     },
 
     pushNotificationsEnabled: true,
